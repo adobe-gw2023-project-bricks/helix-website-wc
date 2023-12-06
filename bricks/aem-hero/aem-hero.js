778a8bfc8b4df70579ecb75ce8e682697143697e
Carlos A. Cabrera <cacabrer@adobe.com>
2023-12-06 13:25:20 -0600

    hero style fixes

--- bricks/aem-hero/aem-hero.js
+++ bricks/aem-hero/aem-hero.js
@@ -5,9 +5,23 @@ export default class Hero extends Brick {
     const image = this.root.querySelector('picture');
     const text = this.root.querySelector('h1').parentElement;
 
+    if (!this.classList.contains('multiple-cta')) {
+      const ctaButton = text.querySelector('a');
+
+      if (ctaButton) {
+        ctaButton.classList.add('button', 'large');
+        ctaButton.closest('p').replaceWith(ctaButton);
+      }
+    } else {
+      const ctaButtonList = text.querySelector('ul');
+      ctaButtonList.classList.add('cta-button-list');
+      const ctaButtons = ctaButtonList.querySelectorAll('ul a');
+      ctaButtons.forEach((btn) => {
+        btn.classList.add('button', 'large', 'black-border');
+      });
+    }
+
     this.shadowRoot.querySelector('slot[name="picture"]').replaceWith(image);
     this.shadowRoot.querySelector('slot[name="text"]').replaceWith(...text.children);
-
-    this.shadowRoot.querySelector('a').classList.add('button', 'primary', 'large');
   }
 }
