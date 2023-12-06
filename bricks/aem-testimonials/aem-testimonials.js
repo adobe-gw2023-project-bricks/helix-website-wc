f0fef451374586e9f7af05160aa30146a222b034
Hannes Hertach <hanneshertach@gmail.com>
2023-12-06 17:46:03 +0100

    add more elements

--- bricks/aem-testimonials/aem-testimonials.js
+++ bricks/aem-testimonials/aem-testimonials.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import { Brick, loadTemplate } from '../../scripts/aem.js';
 
 class Component extends HTMLElement {
@@ -46,10 +47,15 @@ export default class Header extends Brick {
     }
 
     this.root.querySelectorAll(':scope > div').forEach((item) => {
-      const siteLink = slotElement(item.querySelector('a'), 'site-link');
+      const siteLink = slotElement(item.querySelector(':scope > div:nth-child(3) > p:last-of-type'), 'site-link');
       const text = slotElement(item.querySelector(':scope > div:nth-child(3) > p'), 'testimonial-quote');
 
-      [...item.querySelectorAll('x')];
+      const stats = [...item.querySelectorAll('ul > li')]
+        .map((li) => slotElement(li, 'testimonial-stats')).join('');
+
+      const titles = [...item.querySelectorAll(':scope > div:nth-child(3) > p')]
+        .filter((p) => p.childElementCount === 0)
+        .map((p) => slotElement(p, 'titles')).join('');
 
       console.log(item.innerHTML);
       this.append(document.createRange().createContextualFragment(`
@@ -60,6 +66,8 @@ export default class Header extends Brick {
           <picture slot="side-image">${item.querySelectorAll('picture')[1].innerHTML}</picture>
           ${siteLink}
           ${text}
+          ${stats}
+          ${titles}
         </testimonial-slide>
       `));
     });
