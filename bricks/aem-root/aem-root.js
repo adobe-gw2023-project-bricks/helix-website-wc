f91d3d8b0e5fad26bd6e27401bf642124d340431
Alex Carol <acarolcampre@adobe.com>
2023-12-08 17:20:23 +0100

    In-view working for all elements

--- bricks/aem-root/aem-root.js
+++ bricks/aem-root/aem-root.js
@@ -1,5 +1,30 @@
 import { Brick } from '../../scripts/aem.js';
 
+function addBlockLevelInViewAnimation(main) {
+  const observerOptions = {
+    threshold: 0.1, // add `.in-view` class when is 20% in view
+    // rootMargin: '-10px 0px -10px 0px',
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log('intersecting?', entry.target);
+      if (entry.isIntersecting) {
+        console.log('entry in view', entry.target);
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  // support block level animation as well
+  const inviewTriggerClassList = '.fade-up, .fade-in, .fade-left, .fade-right';
+  main.querySelectorAll(inviewTriggerClassList).forEach((section) => {
+    console.log('section selected', section);
+    observer.observe(section);
+  });
+}
+
 export default class Root extends Brick {
   async connectedCallback() {
     // Main Sections
@@ -26,6 +51,8 @@ export default class Root extends Brick {
       wrappers.forEach((wrapper) => section.append(wrapper));
       section.classList.add('section');
     });
+
+    addBlockLevelInViewAnimation(main);
 
     // Set up MutationObserver to detect changes in child node
     this.observer = new MutationObserver((event) => {
